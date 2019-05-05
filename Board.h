7ebe02c216b590d77140af5c0b24d7aa45946112
MacZag98 <38749528+MacZag98@users.noreply.github.com>
2019-05-05 20:30:35 +0200

    Add files via upload
    + struct Field
    + void Board::display()
    + define BOARD_SIZE

--- Board.h
+++ Board.h
@@ -1,10 +1,49 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#define BOARD_SIZE 10
+
+#include <iostream>
+
+struct Field
+{
+	bool is_wall = true;
+	bool is_snake = false;
+	bool is_snake_head = false;
+	bool is_hen = false;
+};
+
 class Board
 {
 public:
+
+	Board() {}
+
+	void display()
+	{
+		for (int i = 0; i < BOARD_SIZE; ++i)
+		{
+			for (int j = 0; j < BOARD_SIZE; ++j)
+			{
+				if (game_board[i][j].is_wall)
+					std::cout << "$";
+				else if (game_board[i][j].is_hen)
+					std::cout << "&";
+				else if (game_board[i][j].is_snake)
+					std::cout << "o";
+				else if (game_board[i][j].is_snake_head)
+					std::cout << "#";
+				else
+					std::cout << " ";
+			}
+			std::cout << "\n";
+		}
+	}
+
 private:
+
+	Field game_board[BOARD_SIZE][BOARD_SIZE];
+
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-main.cpp
+#include <iostream>
+
+#include "Board.h"
+
+int main ()
+{
+	Board game_board;
+	game_board.display();
+	int a;
+	std::cin >> a;
+	return 0;
+}
